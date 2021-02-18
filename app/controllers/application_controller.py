173e72427d9ab0ba5808e1ce4422cbe5d4acad18
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2021-02-18 14:36:21 +0100

    display maintenance informations

--- app/controllers/application_controller.py
+++ app/controllers/application_controller.py
@@ -1,3 +1,4 @@
+import datetime
 import io
 
 from sqlalchemy.orm import sessionmaker
@@ -5,6 +6,7 @@ from flask import render_template
 import matplotlib
 import matplotlib.pyplot as plt
 
+from macpepdb.models.maintenance_information import MaintenanceInformation
 from macpepdb.tasks.statistics import Statistics
 
 from app import app, macpepdb_session, config
@@ -43,10 +45,34 @@ class ApplicationController:
     def dashboard():
         peptide_count, peptide_partitions_svg = ApplicationController.get_peptide_infos(macpepdb_session)
         partition_boundaries = Statistics.get_partition_boundaries(macpepdb_session)
+        digestion_paramters = macpepdb_session.query(MaintenanceInformation).filter(MaintenanceInformation.key == MaintenanceInformation.DIGESTION_PARAMTERS_KEY).one_or_none()
+        if digestion_paramters:
+            digestion_paramters = digestion_paramters.values
+            digestion_paramters['enzyme_name'] = digestion_paramters['enzyme_name'][0].upper() + digestion_paramters['enzyme_name'][1:].lower()
+        else:
+            digestion_paramters = {
+                'enzyme_name': 'n/a',
+                'maximum_number_of_missed_cleavages': 'n/a',
+                'minimum_peptide_length': 'n/a',
+                'maximum_peptide_length': 'n/a'
+            }
+
+        database_status = macpepdb_session.query(MaintenanceInformation).filter(MaintenanceInformation.key == MaintenanceInformation.DATABASE_STATUS_KEY).one_or_none()
+        if database_status:
+            database_status = database_status.values
+            database_status['maintenance_mode'] = "On (updating)" if database_status['maintenance_mode'] else 'Off'
+            database_status['last_update'] = datetime.datetime.utcfromtimestamp(database_status['last_update']).isoformat(sep=' ', timespec='minutes')
+        else:
+            database_status = {
+                'maintenance_mode': 'n/a',
+                'last_update': 'n/a'
+            }
 
         return render_template(
             'application/dashboard.j2',
             peptide_partitions_svg = peptide_partitions_svg,
             peptide_count = peptide_count,
-            partition_boundaries = partition_boundaries
+            partition_boundaries = partition_boundaries,
+            digestion_paramters = digestion_paramters,
+            database_status = database_status
         )
