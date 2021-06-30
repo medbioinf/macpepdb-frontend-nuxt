f2f14fb39c8533300ca6e64d266213e1b67deb59
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2021-06-30 18:56:41 +0200

    indent TOC items on higher levels

--- macpepdb_web_frontend/pages/docs/_slug.vue
+++ macpepdb_web_frontend/pages/docs/_slug.vue
@@ -4,7 +4,7 @@
         <p>{{ article.description }}</p>
         <nav>
             <ul>
-                <li v-for="link of article.toc" :key="link.id">
+                <li v-for="link of article.toc" :key="link.id" :style="`margin-left: ${15 * (link.depth - toc_offset)}px`">
                     <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
                 </li>
             </ul>
@@ -20,6 +20,19 @@ export default {
     async asyncData({ $content, params }) {
         const article = await $content('docs', params.slug).fetch()
         return { article }
+    },
+    computed: {
+        toc_offset(){
+            /* 
+             * TOC depth is defined by the header level (h1, h2, ...) respectively by the number of hashes in markdown.
+             * This method returns depth where the TOC starts.
+             */
+            if(this.article){
+                return Math.min.apply(Math, this.article.toc.map(menu_item => menu_item.depth))
+            } else {
+                return 0
+            }
+        }
     }
 }
 </script>
