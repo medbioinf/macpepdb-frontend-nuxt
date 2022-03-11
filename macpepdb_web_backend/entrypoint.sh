81ccde877aa7fb852400674cc8cde51c879c7590
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2022-03-11 14:51:03 +0000

    fix comment

--- macpepdb_web_backend/entrypoint.sh
+++ macpepdb_web_backend/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "startet with ${@}"
 
-# Prepend 'python ./run.py' arguments given by 'CMD' (dockerfile) or 'command' (docker-compose)
+# Prepend 'python -m macpepdb_web_backend' arguments given by 'CMD' (dockerfile) or 'command' (docker-compose)
 set -- python -m macpepdb_web_backend "$@"
 exec "$@"
